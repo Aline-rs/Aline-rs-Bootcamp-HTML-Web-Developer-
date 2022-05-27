6d018a962df147f162cfc84c7ad2419868a7aa4b
Aline-rs <alinerosa_s@hotmail.com>
2022-05-27 19:57:20 -0300

    Iniciando introdução ao JavaScript

---
+++ Introdução ao JavaScript/js/main.js
@@ -0,0 +1,55 @@
+/*
+var nome = "Aline Rosa";
+var idade = 23;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2)
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"))
+alert(frase.replace("Japão", "Brasil"))
+console.log(frase.toUpperCase) //todas as letras em maiúsculo
+console.log(frase.toLowerCase) // Todas as letras em minusculo
+*/
+
+//var lista = ["maça", "pêra","laranja"]
+//lista.push("uva"); Adiciona um item
+//lista.pop() Retira um item
+//console.log(lista.length) Informa o tamanho do array
+//console.log(lista.reverse) Exibe o array ao contrário
+//console.log(lista.toString) Exibe o conteúdo em formato de string
+//console.log(lista.join(" - ")) Altera o divisor entre as palavras
+/*
+var fruta = {nome: "maça", cor: "vermelha"};
+console.log(fruta);
+console.log(fruta.cor);
+*/
+
+//var frutas = [{nome: "maça", cor: "vermelha"}, {nome:"uva", cor:"roxa"}];
+//console.log(frutas);
+
+/*var idade = prompt("Qual a sua idade?")
+if(idade >= 18){
+    alert("Maior de idade");
+}
+else{
+    alert("Menor de idade")
+};*/
+/*
+var count = 5;
+while (count < 5){
+    console.log(count)
+    count++ //Ele recebe ele mesmo +1 -> é igual a count = count+1
+}*/
+/*
+var count;
+for(count = 1; count <= 5, count++){
+    alert(count);
+}*/
+
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getDay)
+alert(d.getHours)
+alert(d.getMinutes)
