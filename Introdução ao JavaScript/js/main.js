56d707659b832c5fbfe93f9df2fdd07284523977
Aline-rs <alinerosa_s@hotmail.com>
2022-05-29 16:15:04 -0300

    Finalizando Introdução ao JavaScript

--- Introdução ao JavaScript/js/main.js
+++ Introdução ao JavaScript/js/main.js
@@ -46,10 +46,59 @@ while (count < 5){
 var count;
 for(count = 1; count <= 5, count++){
     alert(count);
-}*/
+}
 
 var d = new Date();
 alert(d.getMonth()+1);
 alert(d.getDay)
 alert(d.getHours)
 alert(d.getMinutes)
+
+function soma(n1, n2){
+    return n1 + n2;
+}
+alert(soma(5, 10));
+
+var validar = 0; //Variavel global
+function validaIdade(idade){
+    if (idade >= 18){
+        validar = true
+    }else(
+        validar = false
+    )
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade?")
+validaIdade(idade);
+console.log(validar)
+*/
+
+function clicou(){
+    //alert("Obrigado por clicar!")
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>";
+    //console.log(document.getElementById("agradecimento")
+}
+
+function redirecionar(){
+    window.open("https://globallabs.academy/"); //Abre em outra guia
+    window.location.href = "https://globallabs.academy/" //abre a pagina na mesma guia
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto")
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+function load(){
+    alert("Página carregada")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
